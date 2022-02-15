672573d5c9dd4067edb4dfa87e22cb0452b3dde8
ThomasNeyman <neymanthomas45@gmail.com>
2022-02-15 03:43:37 -0700

    HW 2 import

---
+++ Homework-2/hw2.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from numpy.polynomial.polynomial import Polynomial as ply
+from script.interpolate import lagrange
